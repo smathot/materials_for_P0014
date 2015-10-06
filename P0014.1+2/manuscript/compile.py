dbca00b548d8ccd631994e1464fb0ebaeb2e019b
Sebastiaan Mathot <s.mathot@cogsci.nl>
2015-10-06 11:37:48 +0200

    1.1.1 methods and results

--- P0014.1+2/manuscript/compile.py
+++ P0014.1+2/manuscript/compile.py
@@ -25,16 +25,17 @@ version = '1.1.1'
 build.path += ['svg', 'md', 'tbl']
 build.zoteroApiKey = myZoteroCredentials.zoteroApiKey
 build.zoteroLibraryId = myZoteroCredentials.zoteroLibraryId
-build.setStyle('apa')
+build.setStyle('modern')
 build.tableTemplate = 'pandoc'
 build.pdfHeader = 'Manuscript in preparation [v%s; %s; %s]' % (version, \
 	time.strftime('%c'), git.commitHash().decode())
 if '--snapshot' in sys.argv:
+	git.exportFormats = 'pdf', 'docx'
 	git.snapshot('md/__main__.md', msg=sys.argv[-1])
 else:
 	# build.DOC('md/__main__.md', 'latest-manuscript.doc')
-	# build.ODT('md/__main__.md', 'latest-manuscript.odt')
-	build.PDF('md/__main__.md', 'latest-manuscript.pdf', lineNumbers=False)
+	build.ODT('md/__main__.md', 'latest-manuscript.odt')
+	# build.PDF('md/__main__.md', 'latest-manuscript.pdf', lineNumbers=False)
 	# build.HTML('md/__main__.md', 'latest-manuscript.html')
 	# build.HTML('md/__main__.md', 'latest-manuscript.html', standalone=False)
 	# build.zoteroApiKey = None
